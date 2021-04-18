aaa120709e20a1f17545048204688565042d23dc
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-18 15:34:33 +0200

    adding styles

--- neds_easy_react_bootcamp/src/components/AnimalCard/AnimalCard.js
+++ neds_easy_react_bootcamp/src/components/AnimalCard/AnimalCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import AnimalDetails from '../AnimalDetails/AnimalDetails';
 
 export default function AnimalCard({ diet, name, size }) {
   return(
     <div>
       <h3>{name}</h3>
       <div>{size}kg</div>
-      <div>{diet.join(', ')}.</div>
+      <AnimalDetails
+        diet={diet}
+      />
     </div>
   )
 }
