824de9a1e681aaeb1d83651d03aed44f82ebeeed
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-18 13:03:00 +0200

    Code cleaning

--- neds_easy_react_bootcamp/src/App.js
+++ neds_easy_react_bootcamp/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Neds Easy React Bootcamp.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return null;
 }
 
 export default App;
