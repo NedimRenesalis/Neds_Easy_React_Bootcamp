8d3c92aeab3a01ed22566b13d3587952911dd1f5
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-18 13:23:48 +0200

    Update App.js

--- neds_easy_react_bootcamp/src/App.js
+++ neds_easy_react_bootcamp/src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return null;
+  return (
+   <>
+    <h1>Enjoy the bootcamp</h1>
+    <p>Lets explore JSX</p>
+   </> 
+ )
 }
 
 export default App;
