52a89f6561476ae76d1a489a8b9726fb3501a3b9
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-18 16:10:52 +0200

    component as a prop

--- neds_easy_react_bootcamp/src/components/AnimalCard/AnimalCard.js
+++ neds_easy_react_bootcamp/src/components/AnimalCard/AnimalCard.js
@@ -5,7 +5,7 @@ import AnimalDetails from '../AnimalDetails/AnimalDetails';
 
 export default function AnimalCard({ name, size, ...props }) {
   return(
-    <Card title="Animal">
+    <Card title="Animal" details={<em>Mammal</em>}>
       <h3>{name}</h3>
       <div>{size}kg</div>
       <AnimalDetails
--- neds_easy_react_bootcamp/src/components/Card/Card.js
+++ neds_easy_react_bootcamp/src/components/Card/Card.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
 
-export default function Card({ children, title }) {
+export default function Card({ children, details, title }) {
   return(
     <div className="card">
       <div className="card-details">
         <h2>{title}</h2>
+        {details}
       </div>
       {children}
     </div>
@@ -18,5 +19,10 @@ Card.propTypes = {
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element.isRequired
   ]),
+  details: PropTypes.element,
   title: PropTypes.string.isRequired,
+}
+
+Card.defaultProps = {
+  details: null,
 }
