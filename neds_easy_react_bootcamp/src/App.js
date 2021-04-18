3c7f577dbe74a16d0f0a00394d556973134b07fb
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-18 14:20:48 +0200

    mapping over arrays

--- neds_easy_react_bootcamp/src/App.js
+++ neds_easy_react_bootcamp/src/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import './App.css';
 
 const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face"
+  },
+  {
+    emoji: "🎉",
+    name: "party popper"
+  },
+  {
+    emoji: "💃",
+    name: "woman dancing"
+  }
+];
 
 function App() {
  const greeting = "greeting"; 
@@ -10,27 +24,17 @@ function App() {
     <h1 id={greeting}>Enjoy the bootcamp</h1>
     <p>Lets explore JSX</p>
     <ul>
-        <li>
-            <button
-            onClick={displayEmojiName}
-          >
-              <span role="img" aria-label="grinning face" id="grinning face">😀</span>
-            </button>
-        </li>
-        <li>
-          <button
-            onClick={displayEmojiName}
-          >
-              <span role="img" aria-label="party popper" id="party popper">🎉</span>
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={displayEmojiName}
-          >
-              <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
-          </button>
-        </li>
+       {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
       </ul>
    </div> 
  )
